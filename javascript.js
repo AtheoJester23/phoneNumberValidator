58c7e22289ab8323c9644f3a96f27a3327a625e0
Atheo Jester <atheojetster@gmail.com>
2024-11-24 18:46:34 +0800

    almost done code

--- javascript.js
+++ javascript.js
@@ -1,32 +1,125 @@
 let inputContainer = document.getElementById("results-div");
 let theOutput = "";
 
-document.querySelector(".checkBtn").addEventListener("click", () => {
+document.querySelector(".checkBtn").addEventListener("click", handleValidation);
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    handleValidation();
+  }
+});
+
+function handleValidation() {
   let theInput = document.getElementById("user-input");
   let inputVal = theInput.value.trim();
-  let cleanedVal = inputVal.replace(/[-()]/g, "");
+  let inputValNoSpace = inputVal.replace(/\s/g, "");
+  let cleanedVal = inputVal.replace(/[-()\s]/g, "");
   let lengthVal = cleanedVal.length;
   let splitText = cleanedVal.split("");
+  let splitTextNoClean = inputValNoSpace.split("");
 
-  console.log(splitText);
+  console.log(splitTextNoClean);
+  let count = splitTextNoClean.filter((char) => char === "-").length;
 
   if (inputVal === "") {
     alert("Please provide a phone number");
-  } else if (
-    lengthVal === 11 &&
-    (splitText[0] === "1" || splitText[0] === "5")
-  ) {
-    theOutput += `Valid US number: <p>${inputVal}</p>`;
-    inputContainer.innerHTML = theOutput;
+  } else if (lengthVal === 11 && splitText[0] === "1") {
+    if (inputVal.includes("(") && inputVal.includes(")")) {
+      if (splitTextNoClean[1] === "(" && splitTextNoClean[5] === ")") {
+        if (
+          inputVal.includes("-") &&
+          (splitTextNoClean[6] === "-" || splitTextNoClean[9] === "-")
+        ) {
+          if (splitTextNoClean[9] === "-" && count === 1) {
+            validNum(inputVal, theInput);
+          } else if (
+            splitTextNoClean[6] === "-" &&
+            splitTextNoClean[10] === "-" &&
+            count === 2
+          ) {
+            validNum(inputVal, theInput);
+          } else if (splitTextNoClean[6] === "-" && count === 1) {
+            validNum(inputVal, theInput);
+          } else {
+            notValidNum(inputVal, theInput);
+          }
+        } else {
+          notValidNum(inputVal, theInput);
+        }
+      } else {
+        notValidNum(inputVal, theInput);
+      }
+    } else if (inputVal.includes("-")) {
+      if (
+        splitTextNoClean[4] === "-" ||
+        splitTextNoClean[8] === "-" ||
+        splitTextNoClean[7] === "-"
+      ) {
+        let count = splitTextNoClean.filter((char) => char === "-").length;
 
-    theInput.value = "";
+        if (splitTextNoClean[4] === "-" && count === 1) {
+          validNum(inputVal, theInput);
+        } else if (
+          splitTextNoClean[4] === "-" &&
+          splitTextNoClean[8] === "-" &&
+          count === 2
+        ) {
+          validNum(inputVal, theInput);
+        } else if (splitTextNoClean[7] === "-" && count === 1) {
+          validNum(inputVal, theInput);
+        } else {
+          notValidNum(inputVal, theInput);
+        }
+      } else {
+        notValidNum(inputVal, theInput);
+      }
+    } else {
+      validNum(inputVal, theInput);
+    }
+  } else if (lengthVal === 10) {
+    if (inputVal.includes("(") && inputVal.includes(")")) {
+      if (splitTextNoClean[0] === "(" && splitTextNoClean[4] === ")") {
+        if (inputVal.includes("-")) {
+          if (splitTextNoClean[5] === "-" && count === 1) {
+            validNum(inputVal, theInput);
+          } else if (
+            splitTextNoClean[5] === "-" &&
+            splitTextNoClean[9] === "-" &&
+            count === 2
+          ) {
+            validNum(inputVal, theInput);
+          } else if (splitTextNoClean[8] === "-" && count === 1) {
+            validNum(inputVal, theInput);
+          } else {
+            notValidNum(inputVal, theInput);
+          }
+        } else {
+          validNum(inputVal, theInput);
+        }
+      } else {
+        notValidNum(inputVal, theInput);
+      }
+    } else if (inputVal.includes("-")) {
+      if (splitTextNoClean[3] === "-" && count === 1) {
+        validNum(inputVal, theInput);
+      } else if (splitTextNoClean[6] === "-" && count === 1) {
+        validNum(inputVal, theInput);
+      } else if (
+        splitTextNoClean[3] === "-" &&
+        splitTextNoClean[7] === "-" &&
+        count === 2
+      ) {
+        validNum(inputVal, theInput);
+      } else {
+        notValidNum(inputVal, theInput);
+      }
+    } else {
+      validNum(inputVal, theInput);
+    }
   } else {
-    theOutput += `Invalid US number: <p>${inputVal}</p>`;
-    inputContainer.innerHTML = theOutput;
-
-    theInput.value = "";
+    notValidNum(inputVal, theInput);
   }
-});
+}
 
 document.querySelector(".clearBtn").addEventListener("click", () => {
   let theInput = document.getElementById("user-input");
@@ -36,3 +129,19 @@ document.querySelector(".clearBtn").addEventListener("click", () => {
 
   theInput.value = "";
 });
+
+function validNum(inputVal, theInput) {
+  theOutput += `Valid US number: <p>${inputVal}</p>`;
+  inputContainer.innerHTML = theOutput;
+
+  theInput.value = "";
+  return;
+}
+
+function notValidNum(inputVal, theInput) {
+  theOutput += `Invalid US number: <p>${inputVal}</p>`;
+  inputContainer.innerHTML = theOutput;
+
+  theInput.value = "";
+  return;
+}
